798c7feeee129414204059944709ef6c6bd098d8
Muhammad Ilyas <m.ilyasalfadlih@gmail.com>
2024-06-22 13:27:31 +0700

    Final

--- app/src/main/java/com/example/massive/ui/screen/community/Community.kt
+++ app/src/main/java/com/example/massive/ui/screen/community/Community.kt
@@ -5,10 +5,18 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.Surface
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,12 +34,14 @@ import com.example.massive.data.storage.SharedPreferencesManager
 import com.example.massive.ui.theme.Abu
 import com.example.massive.ui.theme.poppins
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CommunityScreen(navController: NavController) {
+    var searchQuery by remember { mutableStateOf("") }
     val context = LocalContext.current
     val sharedPreferencesManager = SharedPreferencesManager(context)
     val viewModel: CommunityViewModel = viewModel()
-
+    val aduanList = viewModel.aduanList
     val authToken = sharedPreferencesManager.authToken
 
     authToken?.let {
@@ -44,12 +54,32 @@ fun CommunityScreen(navController: NavController) {
             modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center)
         )
     }
-
-    val aduanList = viewModel.aduanList
-
     if (aduanList.isNotEmpty()) {
         LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(aduanList) { aduan ->
+            item {
+                OutlinedTextField(
+                    value = searchQuery,
+                    onValueChange = { newValue ->
+                        searchQuery = newValue
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    placeholder = {
+                        Text("Cari nama pengguna")
+                    },
+                    shape = RoundedCornerShape(20),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color.Black,
+                        focusedLabelColor = Color.Black,
+                        cursorColor = Color.Black,
+                    )
+                )
+            }
+
+            items(aduanList.filter { aduan ->
+                aduan.User.nama_belakang.contains(searchQuery, ignoreCase = true)
+            }) { aduan ->
                 AduanCard(aduan = aduan)
             }
         }
